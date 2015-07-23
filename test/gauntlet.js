994730f2b19d6f55c88f635b7d145911d1e76d17
jwise7 <jwise7@gmail.com>
2015-07-23 15:43:31 -0500

    Fun with html entity attributes
    > character in an attribute causes problems.

--- test/gauntlet.js
+++ test/gauntlet.js
@@ -5,13 +5,20 @@ describe("cleanse-html",function(){
 			var cleanse = require("../cleanse.js");
 			var html = "<p>Hello World</p>";
 			assert.equal("Hello World",cleanse(html));
-			
+		});
+		it('should not permanently set settings that are passed in to the cleanse function',function(){
+			var cleanse = require("../cleanse.js");
 			html = "<head><title>Testing</title></head><p>Hello World</p>";
 			assert.equal("Hello World",cleanse(html));
 			assert.equal("Testing Hello World",cleanse(html,{head:false}));
 			//NOTE: testing to ensure that settings aren't saved between calls.
 			assert.equal("Hello World",cleanse(html));
 		});
+		it('should handle > inside of a html entities attribute',function(){
+			var cleanse = require("../cleanse.js");
+			var html = "<p><img src='...' alt='This image is > most images.' />Hello World</p>";
+			assert.equal("Hello World",cleanse(html));
+		})
 	});
 	describe("configure",function(){
 		it("should allow selective removal and cleansing",function(){
